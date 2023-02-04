a3610f1a96fb0c9b816e534bfdde7d21426689c5
Kamontat Chantrachirathumrong <github@kc.in.th>
2023-02-04 23:45:39 +0700

    fix: flag manager not save flag keys correctly

--- commandline/commands/core.go
+++ commandline/commands/core.go
@@ -5,6 +5,7 @@ import "github.com/kc-workspace/go-lib/logger"
 func New() *Manager {
 	return &Manager{
 		keys:     make([]string, 0),
+		aliases:  make([]string, 0),
 		commands: make(map[string]*Command),
 		logger:   logger.Get("commandline", "command"),
 	}
--- commandline/flags/core.go
+++ commandline/flags/core.go
@@ -3,13 +3,15 @@ package flags
 import "github.com/kc-workspace/go-lib/logger"
 
 func New(flags ...Flag) *Manager {
+	var keys = make([]string, 0)
 	var m = make(map[string]Flag)
 	for _, flag := range flags {
+		keys = append(keys, flag.FlagName())
 		m[flag.FlagName()] = flag
 	}
 
 	return &Manager{
-		keys:  make([]string, 0),
+		keys:  keys,
 		flags: m,
 
 		logger: logger.Get("commandline", "flag"),
