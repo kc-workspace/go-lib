b76153c8fed0e80948f8b9c79b2cd0aee96ed5de
Kamontat Chantrachirathumrong <github@kc.in.th>
2023-02-04 23:47:57 +0700

    perf(cli): override help message to custom message

--- commandline/flags/manager.go
+++ commandline/flags/manager.go
@@ -3,6 +3,7 @@ package flags
 import (
 	"flag"
 	"fmt"
+	"strings"
 
 	"github.com/kc-workspace/go-lib/logger"
 	"github.com/kc-workspace/go-lib/mapper"
@@ -33,6 +34,15 @@ func (m *Manager) Build(name string, args []string) (map[string]mapper.Mapper, [
 		option.Set(key, builder.Parse(flagSet))
 	}
 
+	flagSet.Usage = func() {
+		// Default flag when custom --help not provided
+		m.logger.Log(fmt.Sprintf(
+			"Usage of %s\n%s",
+			name,
+			m.Help(0),
+		))
+	}
+
 	m.logger.Debug("parsing flagset")
 	err := flagSet.Parse(args)
 	if err != nil {
@@ -51,6 +61,76 @@ func (m *Manager) Build(name string, args []string) (map[string]mapper.Mapper, [
 	}
 
 	return result, arguments, err
+}
+
+func (m *Manager) Help(level int) string {
+	const tab = "  "
+	const newline = "\n"
+
+	var indent = tab
+	var maxNameSize = 12
+	var maxLabelSize = 8
+	for _, key := range m.keys {
+		var flag = m.flags[key]
+
+		var nameSize = len(flag.FlagName())
+		if maxNameSize < nameSize {
+			maxNameSize = nameSize
+		}
+		var labelSize = len(flag.FlagValueLabel())
+		if maxLabelSize < labelSize {
+			maxLabelSize = labelSize
+		}
+	}
+
+	for i := 0; i < level; i++ {
+		indent = indent + tab
+	}
+
+	var format = fmt.Sprintf(
+		`%s--%%-%ds %%-%ds %s%s%%s%%s%s`,
+		indent,
+		maxNameSize,
+		maxLabelSize,
+		newline,
+		indent+tab,
+		newline,
+	)
+
+	var builder = strings.Builder{}
+	for _, key := range m.keys {
+		var flag = m.flags[key]
+
+		var alias = ""
+		var flagAliases = flag.FlagAliases()
+		if len(flagAliases) > 0 {
+			var aliases = make([]string, len(flagAliases))
+			for i, alias := range flagAliases {
+				if len(alias) == 1 {
+					aliases[i] = fmt.Sprintf("-%s", alias)
+				} else {
+					aliases[i] = fmt.Sprintf("--%s", alias)
+				}
+			}
+
+			alias = fmt.Sprintf(" %v", aliases)
+		}
+
+		var usage = " <no-description>"
+		if flag.FlagUsage() != "" {
+			usage = fmt.Sprintf(" %s", flag.FlagUsage())
+		}
+
+		builder.WriteString(fmt.Sprintf(
+			format,
+			flag.FlagName(),
+			flag.FlagValueLabel(),
+			alias,
+			usage,
+		))
+	}
+
+	return builder.String()
 }
 
 func (m *Manager) String() string {
