fd37004cbb4a676ad1737eb04664102b37bed2c9
Kamontat Chantrachirathumrong <github@kc.in.th>
2023-04-01 22:09:28 +0700

    perf(xoptional): return object instead of pointer

--- xoptional/new.go
+++ xoptional/new.go
@@ -1,16 +1,16 @@
 package xoptional
 
-func New[T any](t *T) *Optional[T] {
-	return &Optional[T]{
+func New[T any](t *T) Optional[T] {
+	return Optional[T]{
 		value: t,
 	}
 }
 
-func Empty[T any]() *Optional[T] {
+func Empty[T any]() Optional[T] {
 	return New[T](nil)
 }
 
-func Next[T, N any](t *Optional[T], fn func(t T) *N) *Optional[N] {
+func Next[T, N any](t Optional[T], fn func(t T) *N) Optional[N] {
 	if t.Empty() {
 		return Empty[N]()
 	}
--- xoptional/optional.go
+++ xoptional/optional.go
@@ -4,27 +4,27 @@ type Optional[V any] struct {
 	value *V
 }
 
-func (o *Optional[V]) Empty() bool {
+func (o Optional[V]) Empty() bool {
 	return o.value == nil
 }
 
-func (o *Optional[V]) Present() bool {
+func (o Optional[V]) Present() bool {
 	return o.value != nil
 }
 
-func (o *Optional[V]) GetFn(fn func(*V) V) V {
+func (o Optional[V]) GetFn(fn func(*V) V) V {
 	return fn(o.value)
 }
 
-func (o *Optional[V]) Get() V {
+func (o Optional[V]) Get() V {
 	return o.GetFn(func(v *V) V { return *v })
 }
 
-func (o *Optional[V]) Raw() *V {
+func (o Optional[V]) Raw() *V {
 	return o.value
 }
 
-func (o *Optional[V]) Or(fn func() V) V {
+func (o Optional[V]) Or(fn func() V) V {
 	if o.value == nil {
 		return fn()
 	}
@@ -32,6 +32,6 @@ func (o *Optional[V]) Or(fn func() V) V {
 	return *o.value
 }
 
-func (o *Optional[V]) OrElse(def V) V {
+func (o Optional[V]) OrElse(def V) V {
 	return o.Or(func() V { return def })
 }
